fde9a38a9ac528e23aac352a886ace358a29b3d8
sue445 <sue445@sue445.net>
2013-11-24 17:40:19 +0900

    add wrapper

--- script/build_for_jenkins.sh
+++ script/build_for_jenkins.sh
@@ -44,7 +44,7 @@ run "mkdir -m 777 reports/"
 
 #run "RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rspec --profile > ./reports/rspec-console.log"
 #run "ruby ./script/plot-rspec-slowest-examples.rb ./reports/rspec-console.log > ./reports/rspec-plot.csv"
-RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rspec --profile
+run "RAILS_ENV=${JENKINS_RAILS_ENV} bundle exec rspec --profile"
 
 # * if you use mysql_partitioning, use don't use `rake spec`. because primary key is dropped
 # * `rake spec` can not use --profile
